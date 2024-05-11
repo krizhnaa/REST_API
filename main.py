ec5cc7a169d4e00f2778a59c8724779871700a42
krizhnaa <krizhnaprem@gmail.com>
2024-05-11 20:40:03 +0530

    Used PATCH request to update price

--- main.py
+++ main.py
@@ -99,8 +99,20 @@ def add_cafe():
     })
 
 
-
 # HTTP PUT/PATCH - Update Record
+@app.route("/update/<int:cafe_id>", methods=["PATCH"])
+def patch_new_price(cafe_id):
+    new_price = request.args.get("np")
+    cafe = db.session.get(Cafe, cafe_id)
+    print(new_price)
+    print(cafe_id)
+    if cafe:
+        cafe.coffee_price = new_price
+        db.session.commit()
+        return jsonify(response={"success": "Successfully updated the price."}), 200
+    else:
+        return jsonify(error={"Not Found": "Sorry a cafe with that id was not found in the database."}), 404
+
 
 # HTTP DELETE - Delete Record
 
